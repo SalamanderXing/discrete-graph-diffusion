004d46ae0cd56a3a05adc507d3303fe6ce576b52
salamandercrossing <giulio.zani@gmail.com>
2023-08-17 20:20:41 +0000

    venv

--- create_venv.sh
+++ create_venv.sh
@@ -1,8 +1,19 @@
-sudo apt update && sudo apt upgrade
+sudo apt update -y && sudo apt upgrade -y
 
-sudo apt install wget build-essential libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+sudo apt install -y wget build-essential libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
 
 sudo add-apt-repository ppa:deadsnakes/ppa
 
-sudo apt install python3.11
+sudo apt install -y python3.11
 
+wget https://bootstrap.pypa.io/get-pip.py
+
+python3.11 get-pip.py
+
+python3.11 -m pip install virtualenv
+
+python3.11 -m virtualenv venv
+
+source venv/bin/activate
+
+pip install -r requirements.txt
