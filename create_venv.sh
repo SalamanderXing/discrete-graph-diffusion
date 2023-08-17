f2dbe93203bb329f7ae9dd219ec4957459c4dfc9
salamandercrossing <giulio.zani@gmail.com>
2023-08-17 20:13:36 +0000

    venv

---
+++ create_venv.sh
@@ -0,0 +1,8 @@
+sudo apt update && sudo apt upgrade
+
+sudo apt install wget build-essential libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+
+sudo add-apt-repository ppa:deadsnakes/ppa
+
+sudo apt install python3.11
+
